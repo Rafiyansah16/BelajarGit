4a437c1c67fc12544c8c1751cc64809ff209b04b
Rafiyansah16 <142972175+Rafiyansah16@users.noreply.github.com>
2024-01-31 17:42:48 +0700

    Add files via upload

--- latihan1.php
+++ latihan1.php
@@ -1,42 +1,54 @@
-<?php
+<?php 
 
-echo"<h1>HTML dIdalam PHP</h1>";
-echo"<h1>=== Biodata Diri===</h1>";
-echo"<hr>";
-
-echo" Nama Lengkap : Muhammad Rafiyansah";
-echo "<br />";
-echo" Tempat, Tanggal Lahir : 09 Juni 2007";
-echo "<br />";
-echo" No WhatsApp : 085798621070";
-echo "<br />";
-echo" Deskripsi diri : Menjadi Programmer Handal";
-
-
-$nama = "Nama Lengkap : M. Rafiyansah";
-$tanggalLahir = "Tanggal Lahir : 09 Juni 2007";
-$whatsApp = "Nomor WhatsApp : 085798621070";
-$deskripsiDiri = "Deskripsi diri : Menjadi Programmer Handal" ;
-
+$abjad= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+$mapel = ["Pendidikan Agama dan budi Pekerti", "Pendidikan Pancasila", "Bahasa Indonesia", "Matematika", "Bahasa Inggris", "Dasar2 KEjuruan RPL"];
 ?>
-<br>
-<hr>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soal 1</title>
+    <title>Dokumen</title>
+    <style>
+        .bulet{
+            width: 50px;
+            height: 50px;
+            background: red;
+            text-align: center;
+            line-height: 50px;
+            color: #fff;
+            border-radius: 50%;
+            float: left;
+            margin: 5px;
+        }
+
+        .clear{
+            clear: both;
+        }
+        .kotak{
+            width: 150px;
+            height: 150px;
+            background: orange;
+            text-align: center;
+            line-height: 150px;
+            color: #fff;
+            float: left;
+            margin: 5px;
+            border-top-radius: 30%;
+        }
+    </style>
 </head>
 <body>
-    <h1>PHP Di Dalam HTML</h1>
-    <H1>===Biodata Diri===</H1>
-    <div><?= "$nama" ?></div>
-    <div><?= "$tanggalLahir" ?></div>
-    <div><?= "$whatsApp" ?></div>
-    <div><?= "$deskripsiDiri" ?></div>
-    
+    <h1>Abjad A - Z</h1>
+    <?php for($i = 0; $i < count($abjad); $i++) : ?>
+        <div class="bulet"><?= $abjad[$i]; ?></div>
+        <?php endfor; ?>
+
+        <div class="clear"></div>
+    <h1>Mapel RPL </h1>
+        <?php foreach($mapel as $data) : ?>
+            <div class="kotak"><?= $data; ?></div>
+            <?php endforeach; ?>
 </body>
 </html>
